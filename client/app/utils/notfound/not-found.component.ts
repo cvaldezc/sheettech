b554f1afa63f4c614f830d3b9b7885231f9b72b7
Christian Valdez <cvaldezchavez@gmail.com>
2017-09-19 00:05:52 -0500

    add effect into world and add searching also add canvas

--- client/app/utils/notfound/not-found.component.ts
+++ client/app/utils/notfound/not-found.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./not-found.component.sass']
 })
 export class PageNotFoundComponent implements OnInit {
-    land = <HTMLCollectionOf<any>>document.getElementsByClassName('.land')
-    cloud = <HTMLCollectionOf<any>>document.querySelectorAll('.cloud')
 
-    ngOnInit(): void {
+    land: Array<any>
+    cloud: Array<any>
 
+    ngOnInit() {
+        this.land = <any>document.querySelectorAll('.land')
+        this.cloud = <any>document.querySelectorAll('.cloud')
         for (var i = 0; i < this.land.length; i++) {
             this.land[i].style.transform = `translate(${Math.round(Math.random() * 150)}px, ${Math.round(Math.random() * 150)}px)`
             this.land[i].style.width = `${Math.round(Math.random() * 50) + 50 }px`
@@ -20,10 +22,12 @@ export class PageNotFoundComponent implements OnInit {
             this.cloud[i].style.width = `${Math.round(Math.random() * 25) + 25 }px`
         }
 
-        requestAnimationFrame(this.animate)
+       requestAnimationFrame(this.animate)
     }
 
     animate = () => {
+        // console.log(this.land.length)
+
         for (var x = 0; x < this.land.length; x++) {
            this.moved(this.land[x])
            this.moved(this.cloud[x])
@@ -31,17 +35,18 @@ export class PageNotFoundComponent implements OnInit {
         requestAnimationFrame(this.animate)
     }
 
-    moved = (element) => {
-        let s = element.transform.split('(')[1].split(',')
+    moved(element): void {
+        // console.log(element)
+        let s = element.style.transform.split('(')[1].split(',')
         let x = s[0].split('px')[0]
         let y = s[1]
         let w = element.style.width.split('px')[0]
 
         let nx = parseInt(x) - 1
         if (nx + parseInt(w) < -20) {
-            nx = 170
+            nx = 200
         }
-        element.style.transform = `translate(${nx}px, ${y}px)`
+        element.style.transform = `translate(${nx}px, ${y}`
     }
 
 }
